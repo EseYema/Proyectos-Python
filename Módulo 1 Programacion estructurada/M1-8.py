c59c5f06b7ecf6c99faf5296d17dd6e94d7e5b39
EseYema <jpgksi@gmail.com>
2023-12-19 18:38:33 +0100

    creating and updating files

---
+++ Módulo 1 Programacion estructurada/M1-8.py
@@ -0,0 +1,36 @@
+#declaration of variables ** declaracion de variables (M1-8)
+#Link: https://pythones.net/variables-que-son-tipos-de-variables-y-funcion-print-type/
+
+
+print("****************************************************************************************************************")
+
+caja1="Hola Pin Pon"
+caja2=18
+caja3=18.1
+caja4=True
+
+print("La variable caja1 es de tipo ", type(caja1),"y contiene: ", caja1)
+
+print("La variable caja2 es de tipo ", type(caja2), "y contiene: ", caja2)
+
+print("La variable caja3 es de tipo ", type(caja3), "y contiene: ", caja3)
+
+print("La variable caja4 es de tipo ", type(caja4), "y contiene: ", caja4)
+
+print("****************************************************************************************************************")
+print("****************************************************************************************************************")
+
+caja1=22
+caja2=105.9
+caja3=False
+caja4="True"
+
+print("La variable caja1 es de tipo ", type(caja1),"y contiene: ", caja1)
+
+print("La variable caja2 es de tipo ", type(caja2), "y contiene: ", caja2)
+
+print("La variable caja3 es de tipo ", type(caja3), "y contiene: ", caja3)
+
+print("La variable caja4 es de tipo ", type(caja4), "y contiene: ", caja4)
+
+print("****************************************************************************************************************")
