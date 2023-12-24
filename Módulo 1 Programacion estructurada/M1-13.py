4c75b2313b1bfad3b9c4ab5e9a43623e5e63ddaf
EseYema <jpgksi@gmail.com>
2023-12-24 13:45:04 +0100

    Create new file M1-13

---
+++ Módulo 1 Programacion estructurada/M1-13.py
@@ -0,0 +1,5 @@
+#Predefined functions in Python – how to create our own function
+#Funciones predefinidas en Python – como crear nuestra propia función (M1-13)
+#Link: https://pythones.net/funciones-predefinidas-crear-nuestra-funcion/
+
+
